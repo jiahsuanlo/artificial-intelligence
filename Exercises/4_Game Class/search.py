03aa2fcd95f3fd88562571a0abe7dc6d37d2b5b6
jiahsuanlo <jiahsuanlo@yahoo.com>
2018-12-26 22:45:56 -0500

    add test code for iterative deepening search, which is not yet completed...

---
+++ Exercises/4_Game Class/search.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from minimax import minimax_decision
+
+def get_action(gameState, depth_limit):
+    # TODO: Implement a function that calls minimax_decision
+    # for each depth from 1...depth_limit (inclusive of both endpoints)
+    pass
